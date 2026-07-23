f3bf11b354be49bbedd6a5c2962221d65da894a0
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:48:42 +0800

    add file mime detection helper

---
+++ file_type.go
@@ -0,0 +1,15 @@
+package main
+
+import "mime"
+
+func DetectMime(filename string) string {
+ if v:=mime.TypeByExtension("."+extension(filename)); v!="" {return v}
+ return "application/octet-stream"
+}
+
+func extension(name string) string {
+ for i:=len(name)-1;i>=0;i-- {
+  if name[i]=='.' {return name[i+1:]}
+ }
+ return ""
+}
