f5ceb45a6caddb7fd82f43817cb391b759bd0036
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:38:33 +0800

    add history model

---
+++ history.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type History struct {
+ ID int64 `json:"id"`
+ Type string `json:"type"`
+ Direction string `json:"direction"`
+ Content string `json:"content"`
+ Filename string `json:"filename"`
+ Filepath string `json:"filepath"`
+ Mime string `json:"mime"`
+ Size int64 `json:"size"`
+ CreatedAt time.Time `json:"created_at"`
+}
