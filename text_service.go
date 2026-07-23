50f589156edabada1efbefcb0377aa1a5288f699
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:55:35 +0800

    add text transfer service pipeline

---
+++ text_service.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+ "database/sql"
+)
+
+func ReceiveText(db *sql.DB, clipboard *Clipboard, hub *Hub, content string) error {
+ if err:=AddHistory(db, History{
+  Type:"text",
+  Direction:"phone_to_pc",
+  Content:content,
+ }); err!=nil{return err}
+
+ if clipboard!=nil {
+  _=clipboard.Copy(content)
+ }
+
+ if hub!=nil {
+  hub.Broadcast(WSMessage{
+   Type:"text",
+   Content:content,
+  })
+ }
+ return nil
+}
