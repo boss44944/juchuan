da43ccf90434ee8247a9e05c241a6bc7095c8ca8
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 22:24:41 +0800

    upgrade websocket notifications to real websocket connections

--- websocket.go
+++ websocket.go
@@ -4,40 +4,59 @@ import (
  "encoding/json"
  "net/http"
  "sync"
+ "time"
+
+ "github.com/gorilla/websocket"
 )
 
 type WSMessage struct {
  Type string `json:"type"`
  Content string `json:"content,omitempty"`
  URL string `json:"url,omitempty"`
+ Filename string `json:"filename,omitempty"`
 }
 
 type Hub struct {
- clients map[chan []byte]bool
+ clients map[*websocket.Conn]bool
  mu sync.Mutex
 }
 
+var upgrader = websocket.Upgrader{
+ CheckOrigin: func(r *http.Request) bool { return true },
+}
+
 func NewHub()*Hub{
- return &Hub{clients:make(map[chan []byte]bool)}
+ return &Hub{clients:make(map[*websocket.Conn]bool)}
 }
 
 func (h *Hub) Broadcast(v WSMessage){
  b,_:=json.Marshal(v)
  h.mu.Lock()
  defer h.mu.Unlock()
- for c:=range h.clients { c<-b }
+ for c:=range h.clients {
+  _=c.WriteMessage(websocket.TextMessage,b)
+ }
 }
 
 func (h *Hub) Handler(w http.ResponseWriter,r *http.Request){
- ch:=make(chan []byte,8)
- h.mu.Lock(); h.clients[ch]=true; h.mu.Unlock()
- defer func(){h.mu.Lock();delete(h.clients,ch);h.mu.Unlock()}()
+ conn,err:=upgrader.Upgrade(w,r)
+ if err!=nil{return}
+
+ h.mu.Lock()
+ h.clients[conn]=true
+ h.mu.Unlock()
+
+ defer func(){
+  h.mu.Lock()
+  delete(h.clients,conn)
+  h.mu.Unlock()
+  conn.Close()
+ }()
+
  for {
-  select {
-  case b:=<-ch:
-   w.Write(b)
-   if f,ok:=w.(http.Flusher);ok {f.Flush()}
-  case <-r.Context().Done(): return
+  conn.SetReadDeadline(time.Now().Add(60*time.Second))
+  if _,_,err:=conn.ReadMessage();err!=nil{
+   return
   }
  }
 }
