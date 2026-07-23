21d9b53bab3b8e24760f8cc1628afdbc25522f12
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:19:49 +0800

    Build initial Juchuan server entrypoint

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("Juchuan - LAN transfer starting")
+	cfg := loadConfig()
+	if err := saveConfig(cfg); err != nil {
+		log.Printf("save config: %v", err)
+	}
+
+	server := NewServer(cfg)
+	addr := fmt.Sprintf(":%d", cfg.Port)
+
+	log.Printf("Juchuan 菊传 started on http://localhost%s", addr)
+	if err := http.ListenAndServe(addr, server.Handler()); err != nil {
+		log.Fatal(err)
+	}
 }
