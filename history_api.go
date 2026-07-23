efa83d8af7bd21d33e84fab9d088e1c3f4fcf48a
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:58:38 +0800

    connect history api pagination implementation

--- history_api.go
+++ history_api.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+ "database/sql"
  "encoding/json"
  "net/http"
+ "strconv"
 )
 
-func HistoryHandler(w http.ResponseWriter,r *http.Request){
- w.Header().Set("Content-Type","application/json")
- json.NewEncoder(w).Encode([]History{})
+func HistoryHandler(db *sql.DB) http.HandlerFunc {
+ return func(w http.ResponseWriter,r *http.Request){
+  w.Header().Set("Content-Type","application/json")
+
+  page,_:=strconv.Atoi(r.URL.Query().Get("page"))
+  size,_:=strconv.Atoi(r.URL.Query().Get("size"))
+  if page<=0 {page=1}
+  if size<=0 {size=20}
+
+  list,err:=ListHistory(db,page,size)
+  if err!=nil {
+   http.Error(w,err.Error(),500)
+   return
+  }
+
+  json.NewEncoder(w).Encode(list)
+ }
 }
