a3fbaade9b97eb30cc9d7b582b168b6204137fef
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:40:38 +0800

    add text api handler

---
+++ api.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+ "encoding/json"
+ "net/http"
+)
+
+func TextHandler(clipboard *Clipboard) http.HandlerFunc {
+ return func(w http.ResponseWriter,r *http.Request){
+  var req struct{Content string `json:"content"`}
+  if err:=json.NewDecoder(r.Body).Decode(&req);err!=nil {http.Error(w,err.Error(),400);return}
+  if err:=clipboard.Copy(req.Content);err!=nil {http.Error(w,err.Error(),500);return}
+  w.Write([]byte("ok"))
+ }
+}
