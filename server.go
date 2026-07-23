383715353ea44b3fddd81e00ca2f8b16eeec8544
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 21:44:45 +0800

    integrate server configuration and routing

--- server.go
+++ server.go
@@ -1,12 +1,32 @@
 package main
 
 import (
- "fmt"
+ "embed"
  "net/http"
 )
 
-type Server struct { addr string }
+type Server struct {
+ addr string
+ static embed.FS
+ hub *Hub
+}
 
-func NewServer() (*Server,error) { return &Server{addr: ":8000"}, nil }
-func (s *Server) Address() string { return s.addr }
-func (s *Server) Start() error { return http.ListenAndServe(s.addr, http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){fmt.Fprintln(w,"Juchuan") })) }
+func NewServer() (*Server, error) {
+ return &Server{
+  addr: ":8000",
+  hub: NewHub(),
+ }, nil
+}
+
+func (s *Server) Address() string {
+ return s.addr
+}
+
+func (s *Server) Start() error {
+ mux := http.NewServeMux()
+ s.registerRoutes(mux)
+
+ mux.HandleFunc("/ws", s.hub.Handler)
+
+ return http.ListenAndServe(s.addr, mux)
+}
