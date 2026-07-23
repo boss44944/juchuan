1650ff1d966c1de0a73ba772cdf3dd01f4dfb93c
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-23 22:50:43 +0800

    improve startup display and open browser automatically

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import "log"
+import (
+ "log"
+ "strings"
+)
 
 func main() {
  s, err := NewServer()
- if err != nil { log.Fatal(err) }
- log.Printf("Juchuan started: %s", s.Address())
- if err := s.Start(); err != nil { log.Fatal(err) }
+ if err != nil {
+  log.Fatal(err)
+ }
+
+ port := strings.TrimPrefix(s.Address(), ":")
+ url := LocalURL(port)
+
+ log.Println("==============================")
+ log.Println("Juchuan 菊传")
+ log.Println("")
+ log.Println("访问地址:")
+ log.Println(url)
+ log.Println("==============================")
+
+ OpenBrowser(url)
+
+ if err := s.Start(); err != nil {
+  log.Fatal(err)
+ }
 }
