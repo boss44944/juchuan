141978cac3562c2a24c799690868cdefc1de1142
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-27 21:28:05 +0800

    restore system clipboard implementation

--- clipboard.go
+++ clipboard.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
 
 type Clipboard struct{}
 
@@ -14,4 +18,36 @@ func (c *Clipboard) Copy(text string) error {
 
 func (c *Clipboard) String() string {
 	return fmt.Sprint("system clipboard")
+}
+
+func SetClipboard(text string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("pbcopy")
+	case "windows":
+		cmd = exec.Command("clip")
+	default:
+		cmd = exec.Command("sh", "-c", "xclip -selection clipboard")
+	}
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	if _, err := stdin.Write([]byte(text)); err != nil {
+		return err
+	}
+
+	if err := stdin.Close(); err != nil {
+		return err
+	}
+
+	return cmd.Wait()
 }
