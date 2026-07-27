3f482e76a0a777c72e6de183ed35aa83656a3bfb
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-27 21:24:38 +0800

    add clipboard type for services

--- clipboard.go
+++ clipboard.go
@@ -1,32 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"os/exec"
-	"runtime"
-)
+import "fmt"
 
-func SetClipboard(text string) error {
-	var cmd *exec.Cmd
-	switch runtime.GOOS {
-	case "darwin":
-		cmd = exec.Command("pbcopy")
-	case "windows":
-		cmd = exec.Command("clip")
-	default:
-		cmd = exec.Command("sh", "-c", "xclip -selection clipboard")
-	}
-	in, err := cmd.StdinPipe()
-	if err != nil {
-		return err
-	}
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	_, _ = in.Write([]byte(text))
-	_ = in.Close()
-	if err := cmd.Wait(); err != nil {
-		return fmt.Errorf("clipboard: %w", err)
-	}
-	return nil
+type Clipboard struct{}
+
+func (c *Clipboard) Set(text string) error {
+	return SetClipboard(text)
+}
+
+func (c *Clipboard) Copy(text string) error {
+	return c.Set(text)
+}
+
+func (c *Clipboard) String() string {
+	return fmt.Sprint("system clipboard")
 }
