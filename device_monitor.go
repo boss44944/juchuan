67f2ce8ed0dc5bab3a96fbb11b817dbe1649e11a
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-29 00:46:18 +0800

    add device online monitor service

---
+++ device_monitor.go
@@ -0,0 +1,22 @@
+package main
+
+import "time"
+
+func StartDeviceMonitor(manager *DeviceManager, notify func(DeviceEvent)) {
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			now := time.Now().Unix()
+			for _, device := range manager.List() {
+				if now-device.LastSeen > 120 {
+					notify(DeviceEvent{
+						Type: DeviceOfflineEvent,
+						Data: *device,
+					})
+				}
+			}
+		}
+	}()
+}
