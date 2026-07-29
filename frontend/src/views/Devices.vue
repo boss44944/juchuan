06d9fcbf26b4e4bcb1fa0d2c0c09ffbf0e15a2e1
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-29 22:52:54 +0800

    add devices page

---
+++ frontend/src/views/Devices.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-table :data="devices">
+    <el-table-column prop="display_name" label="Device" />
+    <el-table-column prop="platform" label="Platform" />
+    <el-table-column prop="status" label="Status" />
+  </el-table>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import { useDeviceStore } from '../stores/device'
+
+const store = useDeviceStore()
+const devices = computed(() => store.devices)
+
+onMounted(() => {
+  // device loading will be connected with api layer
+})
+</script>
