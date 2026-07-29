3c54b60b585220064bbc697fae9771606b7f37d2
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-29 23:01:43 +0800

    add vue login route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,13 +3,16 @@ import Devices from '../views/Devices.vue'
 import Messages from '../views/Messages.vue'
 import Send from '../views/Send.vue'
 import Config from '../views/Config.vue'
+import Login from '../views/Login.vue'
 
 export default createRouter({
   history:createWebHistory(),
   routes:[
+    {path:'/login', component:Login},
     {path:'/devices', component:Devices},
     {path:'/messages', component:Messages},
     {path:'/send', component:Send},
-    {path:'/config', component:Config}
+    {path:'/config', component:Config},
+    {path:'/', redirect:'/devices'}
   ]
 })
