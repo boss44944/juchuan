a8877f13baa89167326abee0c24a06261129983c
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-29 22:54:01 +0800

    add frontend config page

---
+++ frontend/src/views/Config.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-card>
+    <h2>配置</h2>
+    <el-form>
+      <el-form-item label="服务端口">
+        <el-input v-model="config.port" />
+      </el-form-item>
+      <el-form-item label="自动打开">
+        <el-switch v-model="config.auto_open" />
+      </el-form-item>
+      <el-button @click="save">保存</el-button>
+    </el-form>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { updateConfig } from '../api'
+
+const config = reactive<any>({port:8000, auto_open:false})
+
+async function save(){
+  await updateConfig(config)
+}
+</script>
