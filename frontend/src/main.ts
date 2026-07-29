a9f03094ac0b92a605436df574810ef5d26c0a10
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-29 22:57:21 +0800

    integrate vue i18n plugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,12 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import i18n from './i18n'
+import router from './router'
 
 createApp(App)
   .use(createPinia())
   .use(ElementPlus)
+  .use(i18n)
+  .use(router)
   .mount('#app')
