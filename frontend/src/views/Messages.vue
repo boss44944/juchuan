a4416b324ec880011d58ac52607d96fe56d1609f
boss44944 <34997037+boss44944@users.noreply.github.com>
2026-07-29 22:52:58 +0800

    add messages page

---
+++ frontend/src/views/Messages.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card v-for="item in messages" :key="item.id">
+    <div v-if="item.type === 'TEXT'">{{ item.content }}</div>
+    <div v-else-if="item.type === 'FILE'">
+      <a :href="`/api/file/download/${item.file_id}`">Download</a>
+    </div>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useMessageStore } from '../stores/message'
+
+const store = useMessageStore()
+const messages = computed(() => store.messages)
+</script>
